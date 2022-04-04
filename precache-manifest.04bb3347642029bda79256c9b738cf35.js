8c4a5ad1ec3c5d8fb34a382ec7fb722624f4190c
saurabh <saurabh@thirdrocktechkno.com>
2022-04-04 20:11:31 +0530

    Updates

---
+++ precache-manifest.04bb3347642029bda79256c9b738cf35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230d8585991458ad069601ef1b829490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a72971239d372fe12817",
+    "url": "/static/css/main.27914899.chunk.css"
+  },
+  {
+    "revision": "852bf9bd473ee1fe4d16",
+    "url": "/static/js/2.f146625a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.f146625a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72971239d372fe12817",
+    "url": "/static/js/main.19cf1a9e.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
